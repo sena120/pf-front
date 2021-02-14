1f4a34880b49d05002bc72cc87029a8d665d6677
hanada <hsena120@gmail.com>
2021-02-14 23:11:01 +0900

    make header

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>fodolist</h1>
+
+      <div className={styles.routes}>
+        <Link href="/header/about">
+      <a className={styles.rout}>About</a>
+      </Link>
+
+      <Link href="/header/help">
+        <a className={styles.rout}>Help</a>
+      </Link>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ pages/header/help.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const help = () => {
+  return (
+    <div>
+      this is help
+    </div>
+  )
+}
+
+export default help
